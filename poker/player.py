52bf75c0347f5785373016057e5c602e0d9c02f7
Collin Potts <collinpotts@gmail.com>
2019-06-08 17:11:29 -0500

    Implemented basic player

---
+++ poker/player.py
@@ -0,0 +1,24 @@
+import random
+
+
+class Player(object):
+    def __init__(self, money=0):
+        self.money = money
+
+    def add_money(self, money):
+        assert money >= 0
+        self.money += money
+
+    def sub_money(self, money):
+        assert 0 <= money <= self.money
+        self.money -= money
+
+    def choose_move(self, moves):
+        return random.choice(moves)
+
+
+class ManualPlayer(Player):
+    def choose_move(self, moves):
+        for i, move in enumerate(moves):
+            print('{}: {}'.format(i, move))
+        return moves[int(input('Select a move:'))]
---
+++ poker/texas_holdem.py
@@ -0,0 +1,9 @@
+from poker.cards import StandardDeck
+
+
+class TexasHoldem(object):
+    def __init__(self):
+        self.deck = StandardDeck()
+
+
+
